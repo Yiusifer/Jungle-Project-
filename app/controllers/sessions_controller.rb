3abe213cacd2eba5de4ca98ee68d4ad8b2c9cc3e
Adam Yiu <adam_yiu@outlook.com>
2022-07-04 20:47:25 +0000

    Add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,8 +17,12 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
+    if @current_user == true
+      session[:user_id] = nil
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
   end
 
 
--- app/controllers/users/signup_controller.rb
+++ app/controllers/users/signup_controller.rb
@@ -8,7 +8,7 @@ class Users::SignupController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/', notice: "Account successfully created!"
     else
       render :new
@@ -19,6 +19,6 @@ class Users::SignupController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
